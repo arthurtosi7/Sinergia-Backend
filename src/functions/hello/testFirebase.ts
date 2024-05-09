a61089895de9f8117ca0219ed33f1afc8d7f5db1
Arthur Tosi <arthur.tosi@edu.ufes.br>
2024-05-09 15:02:09 -0300

    Implementando Firebase.2

--- src/functions/hello/testFirebase.ts
+++ src/functions/hello/testFirebase.ts
@@ -1,13 +1,9 @@
 import { APIGatewayProxyEvent, APIGatewayProxyResult } from "aws-lambda";
 import { Handler } from "src/errors/Handler";
 import { ok } from "src/utils/Returns";
-
-// Import the functions you need from the SDKs you need
+import { getFirestore } from "firebase/firestore";
 import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyAtLGZpmFzhi9ydmWjr8W3-C0xCfkpDCw0",
   authDomain: "sinergia-80431.firebaseapp.com",
@@ -17,8 +13,8 @@ const firebaseConfig = {
   appId: "1:845344386673:web:c5909607f556d36b3e75d0"
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
 
 const testFirebase = async (
   event: APIGatewayProxyEvent
