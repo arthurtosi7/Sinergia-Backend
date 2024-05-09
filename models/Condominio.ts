901709a9be944a67077cf5165c6cdaca6ccf0126
Enrico M Caliari <enrico.caliari@edu.ufes.br>
2024-05-09 17:05:55 -0300

    Adicionando modelo de condomínio

--- models/Condominio.ts
+++ models/Condominio.ts
@@ -3,6 +3,20 @@ import Elevador from "./Elevador";
 
 type Condominio = {
     nome: string;
+    bairro: string;
+    cep: string;
+    cidade: string;
+    email: string;
+    federacao: string;
+    logradouro: string;
+    mes_art: string;
+    mes_ria: string;
+    numero: number;
+    rota: string;
+    status_ria: boolean;
+    sub_rota: number;
+    tel_admin: string;
+    tel_sindico: string;
     CVT: CVT[];
     elevador: Elevador;
     //INFORMAÇÕES DO CONDOMÍNIO
