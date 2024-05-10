cf7033749534473b010d5627893f8786530fef04
Enrico M Caliari <enrico.caliari@edu.ufes.br>
2024-05-10 20:38:18 -0300

    Atualizando CVT

--- src/models/CVT.ts
+++ src/models/CVT.ts
@@ -1,5 +1,25 @@
-type CVT = {
+import Condominio from "./Condominio";
+import Elevador from "./Elevador";
 
+type CVT = {
+    num_visita: number;
+    cliente: Condominio;
+    data: string;
+    horario_inicial: string;
+    horario_final: string;
+    endereço: string;
+    tipo_de_visita: string;
+    cnpj: string;
+    tecnico: string;
+    tecnico_auxiliar: string;
+    nome_do_recebedor: string;
+    cpf_do_recebedor: string;
+    temp_paralisacao: string;
+    emprego_de_material: boolean;
+    devolucao_da_chave: boolean;
+    elevador: Elevador;
+    info_adicionais: string;
+    info_tecnica: string;
 };
 
 export default CVT;
