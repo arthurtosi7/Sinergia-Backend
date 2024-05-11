9370878f3a640c99161c062f1b4786bf4386c6ca
Enrico M Caliari <enrico.caliari@edu.ufes.br>
2024-05-11 18:05:06 -0300

    Adicionando function deleteUser (nao consegui corrigir o error em 'delete')

---
+++ src/functions/usuario/deleteUser.ts
@@ -0,0 +1,24 @@
+import { APIGatewayProxyEvent, APIGatewayProxyResult } from "aws-lambda";
+import { Handler } from "src/errors/Handler";
+import UserRepositories from "../../repositories/implementations/UserRepositories";
+import { ok, notFound, badRequest } from "src/utils/Returns";
+
+const deleteUser = async (
+    event: APIGatewayProxyEvent
+) : Promise<APIGatewayProxyResult> => {
+  
+    const { username } = event.pathParameters;
+    if (username === undefined)
+        return  badRequest("Algum campo não definido!");
+
+    const database = new UserRepositories();
+    const user = await database.findByUsername(username);
+    if (user === undefined)
+        return notFound("Usuário não encontrado!");
+
+    database.delete(username);
+        
+    return ok("message", "Usuário deletado com sucesso!");
+};
+
+export const handler = Handler(deleteUser);
