c4558c9f83ad6a78bd78bb220b7c93230e38189a
Enrico M Caliari <enrico.caliari@edu.ufes.br>
2024-05-14 21:47:44 -0300

    Atualizando CondominiosRespositories

--- src/repositories/ICondominiosRepositories.ts
+++ src/repositories/ICondominiosRepositories.ts
@@ -1,7 +1,25 @@
+import CVT from "src/models/CVT";
 import Condominio from "../models/Condominio";
+import Elevador from "src/models/Elevador";
 
 interface ICondominiosRepositories {
+
     findByName(name: string): Promise<Condominio>;
+
+    create(name: string, bairro: string, cep: string, cidade: string, email: string, federacao: string, logradouro: string,
+        mes_art: string, mes_ria: string, numero: number, rota: string, status_ria: boolean, sub_rota: number, tel_admin: string,
+        tel_sindico: string, CVT: CVT[], elevador: Elevador
+    ): Promise<void>;
+
+    update(name: string, bairro: string, cep: string, cidade: string, email: string, federacao: string, logradouro: string,
+        mes_art: string, mes_ria: string, numero: number, rota: string, status_ria: boolean, sub_rota: number, tel_admin: string,
+        tel_sindico: string, CVT: CVT[], elevador: Elevador
+    ): Promise<void>;
+
+    delete(Condominio: string): Promise<void>;
+
+    list(): Promise<Condominio[]>;
+
 }
 
 export default ICondominiosRepositories;
--- src/repositories/implementations/CondominiosRepositories.ts
+++ src/repositories/implementations/CondominiosRepositories.ts
@@ -1,7 +1,9 @@
-import {doc, getDoc, getFirestore} from "firebase/firestore";
+import {doc, getDoc, setDoc, getFirestore, deleteDoc} from "firebase/firestore";
 import { initializeApp } from "firebase/app";
 import ICondominiosRepositories from "../ICondominiosRepositories";
 import Condominio from "../../models/Condominio";
+import CVT from "src/models/CVT";
+import Elevador from "src/models/Elevador";
 
 const firebaseConfig = {
     apiKey: "AIzaSyAtLGZpmFzhi9ydmWjr8W3-C0xCfkpDCw0",
@@ -23,6 +25,62 @@ class CondominiosRepositories implements ICondominiosRepositories {
         }
         const cond = document.data();
         return cond as Condominio;
+    }
+
+    async create(name: string, bairro: string, cep: string, cidade: string, email: string, federacao: string,
+    logradouro: string, mes_art: string, mes_ria: string, numero: number, rota: string, status_ria: boolean,
+    sub_rota: number, tel_admin: string, tel_sindico: string, CVT: CVT[], elevador: Elevador): Promise<void> {
+        await setDoc(doc(this.db, "condominios", name),{
+        name: name,
+        bairro: bairro,
+        cep: cep,
+        cidade: cidade,
+        email: email,
+        federacao: federacao,
+        logradouro: logradouro,
+        mes_art: mes_art,
+        mes_ria: mes_ria,
+        numero: numero,
+        rota: rota,
+        status_ria: status_ria,
+        sub_rota: sub_rota,
+        tel_admin: tel_admin,
+        tel_sindico: tel_sindico,
+        CVT: CVT,
+        elevador: elevador
+        });
+    }
+
+    async update(name: string, bairro: string, cep: string, cidade: string, email: string, federacao: string,
+    logradouro: string, mes_art: string, mes_ria: string, numero: number, rota: string, status_ria: boolean,
+    sub_rota: number, tel_admin: string, tel_sindico: string, CVT: CVT[], elevador: Elevador): Promise<void> {
+        const document = await getDoc(doc(this.db, "condominios", name));
+        if (!document.exists()) {
+            return undefined;
+        }
+        await setDoc(doc(this.db, "condominios", name), {
+            name: name,
+            bairro: bairro,
+            cep: cep,
+            cidade: cidade,
+            email: email,
+            federacao: federacao,
+            logradouro: logradouro,
+            mes_art: mes_art,
+            mes_ria: mes_ria,
+            numero: numero,
+            rota: rota,
+            status_ria: status_ria,
+            sub_rota: sub_rota,
+            tel_admin: tel_admin,
+            tel_sindico: tel_sindico,
+            CVT: CVT,
+            elevador: elevador
+        });
+    }
+
+    async delete(Condominio: string): Promise<void> {
+        await deleteDoc(doc(this.db, "condominios", Condominio));
     }
 
 }
